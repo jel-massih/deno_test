4d46e3bb545f354768dc49cfa1ad8e5d2dc6503e
Jason El-Massih <jel-massih@hotmail.com>
2025-06-17 16:22:59 -0400

    ra

--- todos.ts
+++ todos.ts
@@ -1,6 +1,6 @@
 
 export async function getTestTodo() {
-    const resp = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const resp = await fetch('https://jsonplaceholder.typicode.com/todos/' + Math.floor(Math.random() * 100));
     const data = await resp.json();
     return data;
 }
